6419f4d19bda8b1876c1039c5ade2956802971fd
abhinavjoyc <abhinavjoyc@gmail.com>
2026-03-25 12:08:50 +0530

    implementation of backend of  JWT authentication with DRF
    feat: add JWT authentication with DRF
    
    * Updated settings for DRF, JWT, and CORS
    * Structured project with apps/accounts module
    * Added user registration API with serializer
    * Integrated JWT login using TokenObtainPairView
    * Created protected route with authentication
    * Configured API routing under /api/
    * Installed required dependencies and ran migrations
    
    Implements basic auth flow: register → login → access protected route

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
@@ -115,3 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
